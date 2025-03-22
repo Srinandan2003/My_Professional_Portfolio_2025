bb919bb73fc4d77e167be2aee5fd91abcbad9d12
Srinandan <srinandan2003@gmail.com>
2025-03-22 14:49:31 +0530

    Updated project section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 import project1 from "../assets/projects/project-1.webp";
-
+import project2 from "../assets/projects/project-2.webp"
 export const HERO_CONTENT = `Web Developer skilled in HTML, CSS,
 JavaScript, and frameworks like React,
 Bootstrap, Tailwind, and Chakra UI.
@@ -29,13 +29,31 @@ export const PROJECTS = [
     description:
       "A fully functional task management website with features like calendar scheduling, a focus timer, a Pomodoro timer, a link organizer, and user authentication.",
     technologies: ["HTML", "CSS", "JavaScript", "Tailwind CSS", "Firebase"],
-    text:[
+    text: [
       {text: "Built with modern technologies for performance and scalability."},
-      {text: "Responsive design with smooth animations and transitions."},
-      {text: "User-friendly interface for seamless interactions."}
+      {text: "Developed a Pomodoro timer feature to boost productivity and maintain focus."},
+      {text: "Implemented task prioritization and goal tracking functionalities."},
+      {text: "Integrated calendar for organizing schedules and tracking important dates."},
+      {text: "Utilized Firebase for user authentication and real-time data storage."}
     ],
-    liveLink: "https://fanciful-elf-ed4c09.netlify.app",  // Add your live demo URL here
-    githubLink: "https://github.com/Dheerajmlk/Frontend-fanatics", // Add your GitHub repository URL here
+    liveLink: "https://fanciful-elf-ed4c09.netlify.app",
+    githubLink: "https://github.com/Dheerajmlk/Frontend-fanatics",
+  },
+  {
+    title: "Quick Chat",
+    image: project2,
+    description:
+      "A real-time chat application with instant messaging, user authentication, and a modern UI design. Features include private messaging, JWT authentication, and responsive design.",
+    technologies: ["React", "MongoDB", "Zustand", "Tailwind CSS", "Socket.io", "Express", "DaisyUI"],
+    text: [
+      {text: "Developed real-time chat functionality using Socket.io for instant communication."},
+      {text: "Implemented JWT authentication for secure user login and session management."},
+      {text: "Utilized Zustand for efficient global state management across the application."},
+      {text: "Designed a modern and responsive UI using Tailwind CSS and DaisyUI."},
+      {text: "Integrated MongoDB with Mongoose for robust database management."}
+    ],
+    liveLink: "https://realtime-chat-application-1-ipjp.onrender.com/", // Replace with actual URL
+    githubLink: "https://github.com/Srinandan2003/Realtime_Chat_Application/tree/master", // Replace with actual URL
   },
 ];
 
